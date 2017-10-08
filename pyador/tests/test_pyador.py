499b0cc828958a341e4ca7473b566892c03821ae
Yue Zhao <yuezhao@cs.toronto.edu>
2017-10-08 15:50:00 -0400

    Add more testcases

--- pyador/tests/test_pyador.py
+++ pyador/tests/test_pyador.py
@@ -3,19 +3,29 @@ import unittest
 
 import pandas as pd
 from pyador import pyador
-from pyador.local import DEV_FILE, DEV_DIR
+from pyador.local import TEST_DIR, TEST_FILE
 
-dev_file = os.path.join(DEV_DIR, DEV_FILE)
-df = pd.read_csv(dev_file)
+test_file = os.path.join(TEST_DIR, TEST_FILE)
+test_file = os.path.abspath(os.path.join(os.pardir, test_file))
+
+print(test_file)
+
+df = pd.read_csv(test_file)
 
 
-class MyTestCase(unittest.TestCase):
+class PyadorTestCases(unittest.TestCase):
     def test_argument(self):
-        x = pyador.Pyador(df, 200)
-        self.assertEqual(200, x.n)
+        t1 = pyador.Pyador(df, 200)
+        self.assertEqual(200, t1.n)
 
-        y = pyador.Pyador(df, frac=0.2)
-        self.assertEqual(0.2, y.frac)
+        t2 = pyador.Pyador(df, frac=0.2)
+        self.assertEqual(0.2, t2.frac)
+
+        with self.assertRaises(ValueError):
+            pyador.Pyador(df, frac=1.2)
+
+        with self.assertRaises(ValueError):
+            pyador.Pyador(df, frac=-0.1)
 
         with self.assertRaises(ValueError):
             pyador.Pyador(df, 200, 0.2)
--- pyador/util/tests/test_data_prep.py
+++ pyador/util/tests/test_data_prep.py
@@ -1,17 +1,68 @@
 import unittest
 
-from pyador.util.data_prep import numerical_check
+import pandas as pd
+import numpy as np
+
+from pandas.util.testing import assert_frame_equal
+from pyador.util.data_prep import _numerical_check
+from pyador.util.data_prep import _integrity_check
+from pyador.util.data_prep import _cat_to_num
+from pyador.util.data_prep import _missing_check
+
+# test data
+t_df = pd.DataFrame([[1, "194611010TRH", "NBA", 0],
+                     [1, np.nan, "NBA", 1],
+                     [2, "194611020CHS", "NBA", np.nan],
+                     [2, "194611020CHS", "NBA", 1],
+                     [3, "194611020DTF", "NBA", 0],
+                     [3, "194611020DTF", "NBA", 1],
+                     ], columns=["gameorder", "game_id", "lg_id", "iscopy"
+                                 ])
+t_num_df = pd.DataFrame([[1, 0, 1293.277],
+                         [1, 1, 1306.723],
+                         [2, 0, np.nan],
+                         [np.nan, 1, 1297.071],
+                         [3, 0, 1279.619],
+                         [3, 1, 1320.381],
+                         ], columns=["gameorder", "iscopy", "elo_n"
+                                     ])
+t_series = t_df['gameorder']
+
+t_var = 23333
+t_str = "23333"
 
 
-class MyTestCase(unittest.TestCase):
-    def test_numerical_test(self):
-        self.assertEqual(True, False)
-        # test on series and dataframe
+class UtilTestCases(unittest.TestCase):
+    def test_integrity(self):
+        self.assertEqual(_integrity_check(t_df), True)
 
-    def test_integrity_check(self):
-        self.assertEqual(True, False)
-        # test different dtypes, dataframes and series
+        with self.assertRaises(TypeError):
+            _integrity_check(t_series)
 
+        with self.assertRaises(TypeError):
+            _integrity_check(t_var)
 
-if __name__ == '__main__':
-    unittest.main()
+        with self.assertRaises(TypeError):
+            _integrity_check(t_str)
+
+    def test_numerical_check(self):
+        self.assertEqual(_numerical_check(t_df), False)
+        self.assertEqual(_numerical_check(t_num_df), True)
+
+    # TODO: supplement more testcases for util functions
+    def test_missing_check(self):
+        exp_df = pd.DataFrame([[1, "194611010TRH", "NBA", 0],
+                               [1, "NaN", "NBA", 1],
+                               [2, "194611020CHS", "NBA", 0],
+                               [2, "194611020CHS", "NBA", 1.0],
+                               [3, "194611020DTF", "NBA", 0],
+                               [3, "194611020DTF", "NBA", 1],
+                               ], columns=["gameorder", "game_id", "lg_id",
+                                           "iscopy"])
+        assert_frame_equal(_missing_check(t_df), exp_df)
+
+    def test_cat_to_num(self):
+        self.assertEqual(True, True)
+
+        if __name__ == '__main__':
+            unittest.main()
