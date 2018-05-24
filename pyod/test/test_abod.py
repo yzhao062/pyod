8629c324fa9cdbb00a6a8c2ebba9841307f3a73d
Yue Zhao <yuezhao@cs.toronto.edu>
2018-05-24 17:09:53 -0400

    added more test cases

--- pyod/test/test_abod.py
+++ pyod/test/test_abod.py
@@ -1,32 +1,46 @@
+import os, sys
+
+# temporary solution for relative imports in case pyod is not installed
+# if pyod is installed, no need to use the following line
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import unittest
 from sklearn.utils.testing import assert_equal
 from sklearn.utils.testing import assert_greater
 from sklearn.metrics import roc_auc_score
 
-import sys
-
-# temporary solution for relative imports
-sys.path.append("..")
 from pyod.models.abod import ABOD
 from pyod.utils.load_data import generate_data
 
 
 class TestABOD(unittest.TestCase):
     def setUp(self):
+        self.n_train = 100
+        self.n_test = 50
+        self.contamination = 0.1
+        self.roc_floor = 0.6
         self.X_train, self.y_train, _, self.X_test, self.y_test, _ = generate_data(
-            n_train=100, n_test=50, contamination=0.05)
+            n_train=self.n_train, n_test=self.n_test,
+            contamination=self.contamination)
 
-    def test_abod(self):
-        clf = ABOD(contamination=0.05)
-        clf.fit(self.X_train)
-        assert_equal(len(clf.decision_scores), self.X_train.shape[0])
+        self.clf = clf = ABOD(contamination=self.contamination)
+        self.clf.fit(self.X_train)
 
-        # invert the scores
-        pred_scores = clf.decision_function(self.X_test) * -1
+    def test_train_scores(self):
+        assert_equal(len(self.clf.decision_scores), self.X_train.shape[0])
+
+    def test_prediction_scores(self):
+        pred_scores = self.clf.decision_function(self.X_test) * -1
+
+        # check score shapes
         assert_equal(pred_scores.shape[0], self.X_test.shape[0])
-        assert_equal(clf.predict(self.X_test).shape[0],
-                     self.X_test.shape[0])
-        assert_greater(roc_auc_score(self.y_test, pred_scores), 0.5)
+
+        # check performance
+        assert_greater(roc_auc_score(self.y_test, pred_scores), self.roc_floor)
+
+    def test_prediction_labels(self):
+        pred_labels = self.clf.predict(self.X_test)
+        assert_equal(pred_labels.shape, self.y_test.shape)
 
     def tearDown(self):
         pass
--- pyod/test/test_utility.py
+++ pyod/test/test_utility.py
@@ -1,16 +1,43 @@
+import os, sys
+
+# temporary solution for relative imports in case pyod is not installed
+# if pyod is installed, no need to use the following line
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import unittest
-
-import sys
-
-# temporary solution for relative imports
-sys.path.append("..")
+from sklearn.utils.testing import assert_equal
+from sklearn.utils.testing import assert_allclose
+from sklearn.utils.testing import assert_less_equal
+import numpy as np
 from pyod.utils.load_data import generate_data
 
 
 class TestUtils(unittest.TestCase):
     def setUp(self):
-        self.X_train, self.y_train, _, self.X_test, self.y_test, _ = generate_data(
-            n_train=100, n_test=50, contamination=0.05)
+        self.n_train = 1000
+        self.n_test = 500
+        self.contamination = 0.1
+        pass
+
+    def test_data_generate(self):
+        X_train, y_train, _, X_test, y_test, _ = generate_data(
+            n_train=self.n_train, n_test=self.n_test,
+            contamination=self.contamination)
+
+        assert_equal(y_train.shape[0], X_train.shape[0])
+        assert_equal(y_test.shape[0], X_test.shape[0])
+
+        assert_less_equal(self.n_train - X_train.shape[0], 1)
+        assert_equal(X_train.shape[1], 2)
+
+        assert_less_equal(self.n_test - X_test.shape[0], 1)
+        assert_equal(X_test.shape[1], 2)
+
+        out_perc = np.sum(y_train) / self.n_train
+        assert_allclose(self.contamination, out_perc, atol=0.01)
+
+        out_perc = np.sum(y_test) / self.n_test
+        assert_allclose(self.contamination, out_perc, atol=0.01)
 
     def tearDown(self):
         pass
