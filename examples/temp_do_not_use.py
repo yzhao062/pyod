074db681aa4160fc70080778b382449981aff4b9
Yue Zhao <yuezhao@cs.toronto.edu>
2018-09-02 18:04:39 -0400

    Add Auto-Encoder with Keras (Work-in-progress)

--- examples/temp_do_not_use.py
+++ examples/temp_do_not_use.py
@@ -24,97 +24,118 @@ from pyod.models.cblof import CBLOF
 from pyod.utils.data import generate_data
 from pyod.utils.data import get_color_codes
 from pyod.utils.data import evaluate_print
+from pyod.utils.utility import standardizer
 
 import numpy as np
 import keras
 from keras.datasets import mnist
 from keras.models import Model, Sequential
-from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Flatten, Reshape
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Flatten, Reshape, Dropout
 from keras import regularizers
 
 # Loads the training and test data sets (ignoring class labels)
 #(x_train, _), (x_test, _) = mnist.load_data()
 contamination = 0.1  # percentage of outliers
-n_train = 200  # number of training points
-n_test = 100  # number of testing points
+n_train = 50000  # number of training points
+n_test = 5000  # number of testing points
 
 # Generate sample data
 X_train, y_train, X_test, y_test = \
     generate_data(n_train=n_train,
                   n_test=n_test,
-                  n_features=2,
+                  n_features=400,
                   contamination=contamination,
                   random_state=42)
 
+X_train_norm, X_test_norm = standardizer(X_train, X_test)
+
 #%%
-# Scales the training and test data to range between 0 and 1.
-max_value = float(x_train.max())
-x_train = x_train.astype('float32') / max_value
-x_test = x_test.astype('float32') / max_value
-
-x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-
-(x_train.shape, x_test.shape)
-
-# input dimension = 784
-input_dim = x_train.shape[1]
+# input dimension = 128
+input_dim = X_train.shape[1]
 encoding_dim = 32
+epochs = 10
+batch_size = 32
+dropout_rate = 0.2
 
 compression_factor = float(input_dim) / encoding_dim
 print("Compression factor: %s" % compression_factor)
 
 autoencoder = Sequential()
-autoencoder.add(
-    Dense(encoding_dim, input_shape=(input_dim,), activation='relu')
-)
-autoencoder.add(
-    Dense(input_dim, activation='sigmoid')
-)
+
+#autoencoder.add(
+#    Dense(encoding_dim, input_shape=(input_dim,), activation='relu')
+#)
+#autoencoder.add(
+#    Dense(input_dim, activation='sigmoid')
+#)
+
+autoencoder.add(Dense(64, activation='relu', input_shape=(input_dim,)))
+autoencoder.add(Dropout(dropout_rate))
+#autoencoder.add(Dense(64, activation='relu'))
+#autoencoder.add(Dropout(dropout_rate))
+autoencoder.add(Dense(32, activation='relu'))
+autoencoder.add(Dropout(dropout_rate))
+autoencoder.add(Dense(32, activation='relu'))
+autoencoder.add(Dropout(dropout_rate))
+#autoencoder.add(Dense(64, activation='relu'))
+#autoencoder.add(Dropout(dropout_rate))
+autoencoder.add(Dense(64, activation='relu'))
+autoencoder.add(Dropout(dropout_rate))
+autoencoder.add(Dense(input_dim, activation='sigmoid'))
+autoencoder.compile(loss=keras.losses.mean_squared_error, optimizer='adam')
 
 autoencoder.summary()
 
-input_img = Input(shape=(input_dim,))
-encoder_layer = autoencoder.layers[0]
-encoder = Model(input_img, encoder_layer(input_img))
-
-encoder.summary()
-
-autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
-autoencoder.fit(x_train, x_train,
-                epochs=50,
-                batch_size=256,
-                shuffle=True,
-                validation_data=(x_test, x_test))
+history = autoencoder.fit(X_train_norm, X_train_norm,
+                         epochs=epochs,
+                         batch_size=batch_size,
+                         shuffle=True,
+                         validation_split=0.1,
+                         verbose=1).history
 #%%
-num_images = 10
-np.random.seed(42)
-random_test_images = np.random.randint(x_test.shape[0], size=num_images)
+from sklearn.metrics.pairwise import euclidean_distances
+autoencoder.summary()
+pred_train = autoencoder.predict(X_train_norm)
+pred_test = autoencoder.predict(X_test_norm)
+#%%
+from pyod.utils.stat_models import pairwise_distances_no_broadcast
 
-encoded_imgs = encoder.predict(x_test)
-decoded_imgs = autoencoder.predict(x_test)
+#error_train = euclidean_distances(X_train_norm, pred_train)
+#error_test = cdist(X_test_norm, pred_test, metric='euclidean')
 
-plt.figure(figsize=(18, 4))
+train_error = pairwise_distances_no_broadcast(X_train_norm, pred_train)
+test_error = pairwise_distances_no_broadcast(X_test_norm, pred_test)
 
-#for i, image_idx in enumerate(random_test_images):
-#    # plot original image
-#    ax = plt.subplot(3, num_images, i + 1)
-#    plt.imshow(x_test[image_idx].reshape(28, 28))
-#    plt.gray()
-#    ax.get_xaxis().set_visible(False)
-#    ax.get_yaxis().set_visible(False)
-#    
-#    # plot encoded image
-#    ax = plt.subplot(3, num_images, num_images + i + 1)
-#    plt.imshow(encoded_imgs[image_idx].reshape(8, 4))
-#    plt.gray()
-#    ax.get_xaxis().set_visible(False)
-#    ax.get_yaxis().set_visible(False)
-#
-#    # plot reconstructed image
-#    ax = plt.subplot(3, num_images, 2*num_images + i + 1)
-#    plt.imshow(decoded_imgs[image_idx].reshape(28, 28))
-#    plt.gray()
-#    ax.get_xaxis().set_visible(False)
-#    ax.get_yaxis().set_visible(False)
-#plt.show()
+#%%
+from pyod.models.auto_encoder import AutoEncoder
+contamination = 0.1  # percentage of outliers
+n_train = 50000  # number of training points
+n_test = 5000  # number of testing points
+
+# Generate sample data
+X_train, y_train, X_test, y_test = \
+    generate_data(n_train=n_train,
+                  n_test=n_test,
+                  n_features=400,
+                  contamination=contamination,
+                  random_state=42)
+    
+clf = AutoEncoder(epochs=20)
+clf.fit(X_train)
+pred_scores = clf.decision_scores_
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
