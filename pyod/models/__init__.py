733c035704c16d3840fd430416c645d6bee10a07
yuezhao@cs.toronto.edu <9io9mZ9K#nNL>
2018-09-03 12:50:26 -0400

    Add AutoEncoder

--- pyod/models/__init__.py
+++ pyod/models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from .abod import ABOD
+from .auto_encoder import AutoEncoder
 from .cblof import CBLOF
 from .combination import aom, moa, average, maximization
 from .feature_bagging import FeatureBagging
@@ -12,6 +13,7 @@ from .ocsvm import OCSVM
 from .pca import PCA
 
 __all__ = ['ABOD',
+           'AutoEncoder',
            'CBLOF',
            'aom', 'moa', 'average', 'maximization',
            'FeatureBagging',
