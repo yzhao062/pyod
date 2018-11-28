c9f156f1c1cdca9ea40a701147d74afb50775ef4
Yue Zhao <yuezhao@cs.toronto.edu>
2018-11-28 18:22:44 -0500

    Format improvement and docstring enhancement

--- pyod/models/sos.py
+++ pyod/models/sos.py
@@ -8,6 +8,7 @@ from sklearn.utils.validation import check_is_fitted
 from .base import BaseDetector
 from ..utils.utility import invert_order
 
+
 def get_perplexity(D, beta):
     """Compute the perplexity and the A-row for a specific value of the
     precision of a Gaussian distribution.
@@ -17,6 +18,7 @@ def get_perplexity(D, beta):
     sumA = sum(A)
     H = np.log(sumA) + beta * np.sum(D * A) / sumA
     return H, A
+
 
 class SOS(BaseDetector):
     """Stochastic Outlier Selection.
@@ -98,17 +100,15 @@ class SOS(BaseDetector):
     Reference
     ----------
 
-    Janssens, J., Huszar, F., Postma, E., & van den Herik, H. 
-    Stochastic Outlier Selection. Technical Report TiCC TR 2012-001, 
-    Tilburg University, Tilburg, the Netherlands, 2012.
+    :cite:`janssens2012stochastic`
     """
-    
-    def __init__(self, contamination=0.1, perplexity=30, metric='euclidean', eps=1e-5):
+
+    def __init__(self, contamination=0.1, perplexity=30, metric='euclidean',
+                 eps=1e-5):
         super(SOS, self).__init__(contamination=contamination)
         self.perplexity = perplexity
         self.metric = metric.lower()
         self.eps = eps
-
 
     def x2d(self, X):
         """Computes the dissimilarity matrix of a given dataset.
@@ -128,7 +128,8 @@ class SOS(BaseDetector):
         (n, d) = X.shape
         if self.metric == 'none':
             if n != d:
-                raise ValueError("If you specify 'none' as the metric, the data set "
+                raise ValueError(
+                    "If you specify 'none' as the metric, the data set "
                     "should be a square dissimilarity matrix")
             else:
                 D = X
@@ -137,18 +138,20 @@ class SOS(BaseDetector):
 
             # np.abs protects against extremely small negative values
             # that may arise due to floating point arithmetic errors
-            D = np.sqrt( np.abs(np.add(np.add(-2 * np.dot(X, X.T), sumX).T, sumX)) )
+            D = np.sqrt(
+                np.abs(np.add(np.add(-2 * np.dot(X, X.T), sumX).T, sumX)))
         else:
             try:
                 from scipy.spatial import distance
             except ImportError as e:
-                raise ImportError("Please install scipy if you wish to use a metric "
+                raise ImportError(
+                    "Please install scipy if you wish to use a metric "
                     "other than 'euclidean' or 'none'")
             else:
                 D = distance.squareform(distance.pdist(X, self.metric))
         return D
 
-    def d2a(self, D):     
+    def d2a(self, D):
         """Performs a binary search to get affinities in such a way that each
         conditional Gaussian has the same perplexity. Then returns the
         affinities matrix.
@@ -172,14 +175,15 @@ class SOS(BaseDetector):
         for i in range(n):
             # Compute the Gaussian kernel and entropy for the current precision
             betamin = -np.inf
-            betamax =  np.inf
-            Di = D[i, np.concatenate((np.r_[0:i], np.r_[i+1:n]))]
+            betamax = np.inf
+            Di = D[i, np.concatenate((np.r_[0:i], np.r_[i + 1:n]))]
             (H, thisA) = get_perplexity(Di, beta[i])
 
             # Evaluate whether the perplexity is within tolerance
             Hdiff = H - logU
             tries = 0
-            while (np.isnan(Hdiff) or np.abs(Hdiff) > self.eps) and tries < 5000:
+            while (np.isnan(Hdiff) or np.abs(
+                    Hdiff) > self.eps) and tries < 5000:
                 if np.isnan(Hdiff):
                     beta[i] = beta[i] / 10.0
                 # If not, increase or decrease precision
@@ -201,7 +205,7 @@ class SOS(BaseDetector):
                 tries += 1
 
             # Set the final row of A
-            A[i, np.concatenate((np.r_[0:i], np.r_[i+1:n]))] = thisA
+            A[i, np.concatenate((np.r_[0:i], np.r_[i + 1:n]))] = thisA
 
         return A
 
@@ -219,8 +223,8 @@ class SOS(BaseDetector):
         B : array, shape (n_samples, )
             Returns the matrix of binding probabilities.       
         """
-        
-        B = A/A.sum(axis=1)[:,np.newaxis]
+
+        B = A / A.sum(axis=1)[:, np.newaxis]
         return B
 
     def b2o(self, B):
@@ -237,7 +241,7 @@ class SOS(BaseDetector):
         B : array, shape (n_samples, )
             Returns the matrix of binding probabilities.       
         """
-        O = np.prod(1-B, 0)
+        O = np.prod(1 - B, 0)
         return O
 
     def fit(self, X, y=None):
