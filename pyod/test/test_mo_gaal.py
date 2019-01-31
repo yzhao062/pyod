e2dbf35e2e709c27102c15fad00e91cc8b5cddf2
Yue Zhao <yuezhao@cs.toronto.edu>
2019-01-31 11:06:42 -0500

    Refactor to speed up test (GAN is not stable anyway)

--- pyod/test/test_mo_gaal.py
+++ pyod/test/test_mo_gaal.py
@@ -24,12 +24,17 @@ from pyod.utils.data import generate_data
 
 
 class TestMO_GAAL(unittest.TestCase):
+    """
+    Notes: GAN may yield unstable results, so the test is design for running
+    models only, without any performance check.
+    """
+
     def setUp(self):
-        self.n_train = 3000
-        self.n_test = 1000
-        self.n_features = 10
+        self.n_train = 1000
+        self.n_test = 200
+        self.n_features = 2
         self.contamination = 0.1
-        # TODO: GAN may yield unstable results; turning performance check off
+        # GAN may yield unstable results; turning performance check off
         # self.roc_floor = 0.8
         self.X_train, self.y_train, self.X_test, self.y_test = generate_data(
             n_train=self.n_train, n_test=self.n_test,
