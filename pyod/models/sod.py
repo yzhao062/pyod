24d96c7ec2d80322ceb7a084199b891c9ebf88b9
Yahya <yalmardeny@tssg,org>
2019-03-12 12:38:34 +0000

    availing of numpy isin function to speed up execustion

--- pyod/models/sod.py
+++ pyod/models/sod.py
@@ -136,16 +136,16 @@ class SOD(BaseDetector):
         _count = np.zeros(shape=(n, n), dtype=np.uint16)
         # Count the distance using the customized function
         for i in range(n):
-            # get the previous cached result.
             # The point should not be in its reference set,
             # but we need it temporarily to maintain indices order,
             # it has the max possible value: 65535 (max unsigned int16)
             # so it'll be always as first index
-            _count[i:, range(i+1)] = list(_count[range(i), i]) + [np.iinfo(np.uint16).max]
-            for j in range(i+1, n):
-                _count[i, j] = _dist(ind[i], ind[j])
+            temp = np.sum(np.isin(ind, ind[i]), axis=1).ravel()
+            temp[i] = np.iinfo(np.uint16).max
+            _count[i] = temp
 
-        return np.flip(np.argsort(_count), axis=1)[:, range(1, self.ref_set+1)]
+        return np.flip(np.argsort(_count), axis=1)[:, range(1, self.ref_set + 1)]
+
 
     def _sod(self):
         """
