3b2f795d3231afd3697bfba49f050b89d0d2ab7e
Yahya <yalmardeny@tssg,org>
2019-04-14 13:42:49 +0100

    rebasing branch

--- docs/rebuild.bat
+++
@@ -1,7 +0,0 @@
-REM rebuild docs shortcut
-cd..
-xcopy examples\*.png docs\figs /Y
-xcopy notebooks\*.csv docs\tables\ /Y
-cd docs
-call make clean
-call make html
--- pyod/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models
-from . import utils
-# TODO: add version information here
-
-__all__ = ['models', 'utils']
