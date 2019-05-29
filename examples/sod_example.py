4997319470f8341076c7efb9805bed457e75cd90
Yahya <yalmardeny@tssg,org>
2019-05-29 23:50:40 +0100

    make it compatible with Python 2.7

--- examples/sod_example.py
+++ examples/sod_example.py
@@ -179,4 +179,4 @@ if __name__ == "__main__":
 
     # visualize the results
     visualize(clf_name, X_train, y_train, X_test, y_test, y_train_pred,
-              y_test_pred, show_figure=True, save_figure=True)
+              y_test_pred, show_figure=True, save_figure=False)
