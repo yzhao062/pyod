23abd662d7546868e0c9c2a1aef092dca3df0457
Yue Zhao <zhaoy@cmu.edu>
2019-06-12 12:04:02 -0400

    pre-release of SOD

--- pyod/test/test_sod.py
+++ pyod/test/test_sod.py
@@ -32,7 +32,7 @@ class TestSOD(unittest.TestCase):
         self.n_train = 100
         self.n_test = 50
         self.contamination = 0.1
-        self.roc_floor = 0.6
+        self.roc_floor = 0.8
         self.X_train, self.y_train, self.X_test, self.y_test = generate_data(
             n_train=self.n_train, n_test=self.n_test,
             contamination=self.contamination, random_state=42)
