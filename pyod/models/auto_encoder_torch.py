884556b7ba866bf5c7836a0399c96447973d9480
ForestsKing <60333390+ForestsKing@users.noreply.github.com>
2021-07-21 20:26:02 +0800

    correct the PyODDataset
    In the original PyODDataset, if preprocessing=false, the error [AttributeError: 'NoneType' object has no attribute 'any'] will be reported.After correction, the problem has been solved.

--- pyod/models/auto_encoder_torch.py
+++ pyod/models/auto_encoder_torch.py
@@ -38,7 +38,7 @@ class PyODDataset(torch.utils.data.Dataset):
             idx = idx.tolist()
         sample = self.X[idx, :]
 
-        if self.mean.any():
+        if type(self.mean) is np.ndarray:
             sample = (sample - self.mean) / self.std
 
         return torch.from_numpy(sample), idx
@@ -211,19 +211,19 @@ class AutoEncoder(BaseDetector):
                  hidden_neurons=None,
                  hidden_activation='relu',
                  batch_norm=True,
-                 # loss='mse', 
+                 # loss='mse',
                  # optimizer='adam',
                  learning_rate=1e-3,
                  epochs=100,
                  batch_size=32,
                  dropout_rate=0.2,
-                 # l2_regularizer=0.1, 
+                 # l2_regularizer=0.1,
                  weight_decay=1e-5,
-                 # validation_size=0.1, 
+                 # validation_size=0.1,
                  preprocessing=True,
                  loss_fn=None,
-                 # verbose=1, 
-                 # random_state=None, 
+                 # verbose=1,
+                 # random_state=None,
                  contamination=0.1,
                  device=None):
         super(AutoEncoder, self).__init__(contamination=contamination)
