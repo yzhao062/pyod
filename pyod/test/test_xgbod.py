10ff7fd61fca64221fef7998b64d95d2defa7099
Yue Zhao <yzhao062@gmail.com>
2021-10-27 12:52:12 -0400

    revert test

--- pyod/test/test_xgbod.py
+++ pyod/test/test_xgbod.py
@@ -94,38 +94,38 @@ class TestXGBOD(unittest.TestCase):
         assert (pred_proba.min() >= 0)
         assert (pred_proba.max() <= 1)
 
-    def test_prediction_proba_linear(self):
-        pred_proba = self.clf.predict_proba(self.X_test, method='linear')
-        assert (pred_proba.min() >= 0)
-        assert (pred_proba.max() <= 1)
+    # def test_prediction_proba_linear(self):
+    #     pred_proba = self.clf.predict_proba(self.X_test, method='linear')
+    #     assert (pred_proba.min() >= 0)
+    #     assert (pred_proba.max() <= 1)
+    #
+    # def test_prediction_proba_unify(self):
+    #     pred_proba = self.clf.predict_proba(self.X_test, method='unify')
+    #     assert (pred_proba.min() >= 0)
+    #     assert (pred_proba.max() <= 1)
+    #
+    # def test_prediction_proba_parameter(self):
+    #     with assert_raises(ValueError):
+    #         self.clf.predict_proba(self.X_test, method='something')
 
-    def test_prediction_proba_unify(self):
-        pred_proba = self.clf.predict_proba(self.X_test, method='unify')
-        assert (pred_proba.min() >= 0)
-        assert (pred_proba.max() <= 1)
-
-    def test_prediction_proba_parameter(self):
-        with assert_raises(ValueError):
-            self.clf.predict_proba(self.X_test, method='something')
-
-    def test_prediction_labels_confidence(self):
-        pred_labels, confidence = self.clf.predict(self.X_test,
-                                                   return_confidence=True)
-        assert_equal(pred_labels.shape, self.y_test.shape)
-        assert_equal(confidence.shape, self.y_test.shape)
-        assert (confidence.min() >= 0)
-        assert (confidence.max() <= 1)
-
-    def test_prediction_proba_linear_confidence(self):
-        pred_proba, confidence = self.clf.predict_proba(self.X_test,
-                                                        method='linear',
-                                                        return_confidence=True)
-        assert (pred_proba.min() >= 0)
-        assert (pred_proba.max() <= 1)
-
-        assert_equal(confidence.shape, self.y_test.shape)
-        assert (confidence.min() >= 0)
-        assert (confidence.max() <= 1)
+    # def test_prediction_labels_confidence(self):
+    #     pred_labels, confidence = self.clf.predict(self.X_test,
+    #                                                return_confidence=True)
+    #     assert_equal(pred_labels.shape, self.y_test.shape)
+    #     assert_equal(confidence.shape, self.y_test.shape)
+    #     assert (confidence.min() >= 0)
+    #     assert (confidence.max() <= 1)
+    #
+    # def test_prediction_proba_linear_confidence(self):
+    #     pred_proba, confidence = self.clf.predict_proba(self.X_test,
+    #                                                     method='linear',
+    #                                                     return_confidence=True)
+    #     assert (pred_proba.min() >= 0)
+    #     assert (pred_proba.max() <= 1)
+    #
+    #     assert_equal(confidence.shape, self.y_test.shape)
+    #     assert (confidence.min() >= 0)
+    #     assert (confidence.max() <= 1)
 
     def test_fit_predict(self):
         pred_labels = self.clf.fit_predict(self.X_train, self.y_train)
