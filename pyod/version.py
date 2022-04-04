80ab7f537532c1cea683317e925853e75ac21d48
yzhao062 <QUV4&g4VTx9V>
2022-04-03 21:04:22 -0400

    v0.9.9 pre-release

--- pyod/version.py
+++ pyod/version.py
@@ -20,4 +20,4 @@
 # Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
 # 'X.Y.dev0' is the canonical version of 'X.Y.dev'
 #
-__version__ = '0.9.8'  # pragma: no cover
+__version__ = '0.9.9'  # pragma: no cover
