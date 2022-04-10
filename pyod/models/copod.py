5b7023d1df1976f4656c5acf2fa9bb56aa28675e
Ben Thomas <77925371+ben-thomas1@users.noreply.github.com>
2022-04-10 04:16:27 +0100

    Add verbose flag to COPOD model

--- pyod/models/copod.py
+++ pyod/models/copod.py
@@ -79,6 +79,10 @@ class COPOD(BaseDetector):
         The number of jobs to run in parallel for both `fit` and
         `predict`. If -1, then the number of jobs is set to the
         number of cores.
+    
+    verbose : bool (default=True)
+        If True, information about the Joblib backend will be printed at
+        runtime.
 
     Attributes
     ----------
