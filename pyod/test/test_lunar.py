8d29b509ad1f543e299562af0bc464dde67685f9
Yue Zhao <yzhao062@gmail.com>
2022-07-05 12:31:59 -0400

    update data generation

--- pyod/test/test_lunar.py
+++ pyod/test/test_lunar.py
@@ -31,7 +31,7 @@ class TestLUNAR(unittest.TestCase):
         self.n_features = 10
         self.contamination = 0.1
         self.roc_floor = 0.8
-        self.X_train, self.y_train, self.X_test, self.y_test = generate_data(
+        self.X_train, self.X_test, self.y_train, self.y_test = generate_data(
             n_train=self.n_train, n_test=self.n_test,
             n_features=self.n_features, contamination=self.contamination,
             random_state=42)
