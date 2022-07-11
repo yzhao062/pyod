395e574c1cc7679e1d85b6e36bc416ef5517064f
Michiel <michielbongaerts@fastmail.com>
2022-07-11 12:43:47 +0200

    changed order of train and test X,y

--- pyod/test/test_alad.py
+++ pyod/test/test_alad.py
@@ -30,7 +30,7 @@ class TestALAD(unittest.TestCase):
         self.n_features = 2
         self.contamination = 0.1
         self.roc_floor = 0.8
-        self.X_train, self.y_train, self.X_test, self.y_test = generate_data(
+        self.X_train, self.X_test, self.y_train, self.y_test = generate_data(
             n_train=self.n_train, n_test=self.n_test,
             n_features=self.n_features, contamination=self.contamination,
             random_state=42)
