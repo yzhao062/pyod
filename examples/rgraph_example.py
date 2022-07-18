2a5c24368aaffb123271ea4e87a8b13e082e580e
Michiel <michielbongaerts@fastmail.com>
2022-07-18 11:37:38 +0200

    Small changes in settings for examples

--- examples/rgraph_example.py
+++ examples/rgraph_example.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
 
     contamination = 0.1  # percentage of outliers
     n_train = 100  # number of training points
-    n_test = 25  # number of testing points
+    n_test = 100  # number of testing points
 
     # Generate sample data
     X_train, X_test, y_train, y_test = generate_data(
@@ -37,26 +37,16 @@ if __name__ == "__main__":
         random_state=42,
     )
 
-<<<<<<< HEAD:examples/rgraph_example.py
+
     # train R-graph detector
     clf_name = 'R-graph'
-    clf = RGraph(n_nonzero = 100, transition_steps = 20 , gamma = 50, blocksize_test_data = 5,
+    clf = RGraph(n_nonzero = 100, transition_steps = 20 , gamma = 50, blocksize_test_data = 20,
                  tau = 1, preprocessing=True, active_support = False, gamma_nz = False,
-                 algorithm= 'lasso_lars', maxiter= 40, verbose =1 )
+                 algorithm= 'lasso_lars', maxiter= 100, verbose =1 )
 
     clf.fit(X_train)
 
 
-=======
-    # train AutoEncoder detector
-    clf_name = 'AnoGAN'
-    clf = AnoGAN(G_layers=[10, 20], D_layers=[20, 2],
-                 preprocessing=True, index_D_layer_for_recon_error=1,
-                 epochs=200, contamination=contamination, verbose=1)
-
-    clf.fit(X_train)
-
->>>>>>> c2839e0c6acd08b219398804326b43ba3e262af4:examples/anogan_example.py
     # get the prediction labels and outlier scores of the training data
     y_train_pred = clf.labels_  # binary labels (0: inliers, 1: outliers)
     y_train_scores = clf.decision_scores_  # raw outlier scores
@@ -70,14 +60,6 @@ if __name__ == "__main__":
     evaluate_print(clf_name, y_train, y_train_scores)
     print("\nOn Test Data:")
     evaluate_print(clf_name, y_test, y_test_scores)
-<<<<<<< HEAD:examples/rgraph_example.py
 
 
-=======
 
-    # visualize the results
-    visualize(clf_name, X_train, y_train, X_test, y_test, y_train_pred,
-              y_test_pred, show_figure=True, save_figure=False)
-
-    clf.plot_learning_curves(window_smoothening=30)
->>>>>>> c2839e0c6acd08b219398804326b43ba3e262af4:examples/anogan_example.py
