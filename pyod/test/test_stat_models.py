44492b4b0c27fa9c447668eeea5fedfd51d73426
Lucas Weber <lucas@CS6.FAU.DE>
2022-12-20 17:07:32 +0100

    Implemented own ecdf estimator (faster runtime, less dependencies) including a test

--- pyod/test/test_stat_models.py
+++ pyod/test/test_stat_models.py
@@ -20,6 +20,9 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from pyod.utils.stat_models import pairwise_distances_no_broadcast
 from pyod.utils.stat_models import wpearsonr
 from pyod.utils.stat_models import pearsonr_mat
+from pyod.utils.stat_models import column_ecdf
+import statsmodels.distributions
+import time
 
 
 class TestStatModels(unittest.TestCase):
@@ -39,6 +42,10 @@ class TestStatModels(unittest.TestCase):
                            [3, 3, 3],
                            [4, 4, 3],
                            [0, 1, 2]])
+
+        # trigger the njit compiler for one of the functions
+        # in the stat models packages
+        column_ecdf(self.mat)
 
     def test_pairwise_distances_no_broadcast(self):
         assert_allclose(pairwise_distances_no_broadcast(self.X, self.Y),
@@ -64,6 +71,51 @@ class TestStatModels(unittest.TestCase):
 
         assert (np.min(pear_mat) >= -1)
         assert (np.max(pear_mat) <= 1)
+
+    def test_column_ecdf(self):
+
+        def ecdf(X):
+            """Calculated the empirical CDF of a given dataset using the statsmodels function.
+            Parameters
+            ----------
+            X : numpy array of shape (n_samples, n_features)
+                The training dataset.
+            Returns
+            -------
+            ecdf(X) : float
+                Empirical CDF of X
+            """
+            ecdf = statsmodels.distributions.ECDF(X)
+            return ecdf(X)
+
+        # run a test case that has equal elements per feature column to show also
+        # this highly unlikely case works
+        mat = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1], [2, 2, 2]])
+        assert_equal(column_ecdf(mat), np.apply_along_axis(ecdf, 0, mat))
+
+        # run the models multiple times for random matrices
+        new = []
+        old = []
+        for _ in range(50):
+
+            # create random matrix for testing
+            mat = np.random.rand(1000, 100)
+
+            # execute and measure the time of our own function
+            t = time.time()
+            result = column_ecdf(mat)
+            new.append(time.time() - t)
+
+            # execute the statsmodels function and measure execution time
+            t = time.time()
+            expected = np.apply_along_axis(ecdf, 0, mat)
+            old.append(time.time() - t)
+
+            # check that the results are equal
+            assert_equal(result, expected)
+
+        print(f'Statsmodels ECDF took {sum(old)/len(old)*1000:0.1f} ms '
+              f'and own implementation {sum(new)/len(new)*1000:0.1f} ms per run.')
 
     def tearDown(self):
         pass
