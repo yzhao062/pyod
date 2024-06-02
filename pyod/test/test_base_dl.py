5c264ea3708dcd0dcf168b9a6be3a753b4a68e0d
RaymondY <raymondyangtk@gmail.com>
2024-06-02 02:56:30 -0700

    Fix bugs and clarify the variable names.

--- pyod/test/test_base_dl.py
+++ pyod/test/test_base_dl.py
@@ -42,14 +42,15 @@ class DummyUnchangeModel(nn.Module):
 
 
 class DummyDetector(BaseDeepLearningDetector):
-    def __init__(self, contamination=0.1, epoch_num=1, optimizer='adam', 
-                 loss_func=None, criterion='mse', verbose=1, 
-                 preprocessing=True, use_compile=False):
+    def __init__(self, contamination=0.1, epoch_num=1, optimizer_name='adam', 
+                 loss_func=None, criterion=None, criterion_name='mse', 
+                 verbose=1, preprocessing=True, use_compile=False):
         super(DummyDetector, self).__init__(contamination=contamination,
                                             epoch_num=epoch_num,
-                                            optimizer=optimizer,
+                                            optimizer_name=optimizer_name,
                                             loss_func=loss_func,
                                             criterion=criterion,
+                                            criterion_name=criterion_name,
                                             verbose=verbose,
                                             preprocessing=preprocessing,
                                             use_compile=use_compile)
@@ -62,10 +63,7 @@ class DummyDetector(BaseDeepLearningDetector):
         x = x.to(self.device)
         self.optimizer.zero_grad()
         output = self.model(x)
-        if hasattr(self.model, 'loss_func'):
-            loss = self.loss_func(output, x)
-        else:
-            loss = self.criterion(output, x)
+        loss = self.criterion(output, x)
         loss.backward()
         self.optimizer.step()
         return loss.item()
@@ -94,28 +92,29 @@ class TestBaseDL(unittest.TestCase):
         dummy_clf.fit(self.X_train)
         self.assertEqual(dummy_clf.contamination, 0.1)
         self.assertIsInstance(dummy_clf.optimizer, torch.optim.Adam)
-        self.assertEqual(dummy_clf.loss_func, None)
         self.assertIsInstance(dummy_clf.criterion, nn.MSELoss)
         
-        dummy_clf = DummyDetector(contamination=0.2, optimizer='sgd', loss_func=loss_function, criterion='mae')
+        dummy_clf = DummyDetector(contamination=0.2, optimizer_name='sgd', 
+                                  loss_func=loss_function, criterion='mae')
         dummy_clf.fit(self.X_train)
         self.assertEqual(dummy_clf.contamination, 0.2)
         self.assertIsInstance(dummy_clf.optimizer, torch.optim.SGD)
-        self.assertEqual(dummy_clf.loss_func, loss_function)
-
-        dummy_clf = DummyDetector(criterion='mae')
-        self.assertIsInstance(dummy_clf.criterion, nn.L1Loss)
+        self.assertEqual(dummy_clf.criterion, loss_function)
 
         dummy_clf = DummyDetector(criterion=DummyLoss())
         self.assertIsInstance(dummy_clf.criterion, DummyLoss)
 
+        dummy_clf = DummyDetector(criterion_name='mae')
+        self.assertIsInstance(dummy_clf.criterion, nn.L1Loss)
+
         self.assertRaises(ValueError, DummyDetector, contamination=0)
         self.assertRaises(ValueError, DummyDetector, contamination=0.51)
         with self.assertRaises(ValueError):
-            dummy_clf = DummyDetector(optimizer='dummy_optimizer')
+            dummy_clf = DummyDetector(optimizer_name='dummy_optimizer')
             dummy_clf.fit(self.X_train)
         self.assertRaises(ValueError, DummyDetector, loss_func=0)
         self.assertRaises(ValueError, DummyDetector, criterion=0)
+        self.assertRaises(ValueError, DummyDetector, criterion_name='dummy_criterion')
 
     def test_fit_decision_function(self):
         zero_scores = np.zeros(self.n_train)
