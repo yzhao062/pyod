7208100e8cffa09bbb1636e067d7a4b470bcba4b
zhuox5 <zhuox5@uci.edu>
2024-06-03 18:02:13 -0700

    reuse gaal_base to make sure coverage >= 94%

--- pyod/models/mo_gaal.py
+++ pyod/models/mo_gaal.py
@@ -20,34 +20,6 @@ from .base import BaseDetector
 from .gaal_base import create_discriminator, create_generator
 
 
-# class Generator(nn.Module):
-#     def __init__(self, latent_size):
-#         super(Generator, self).__init__()
-#         self.layer1 = nn.Linear(latent_size, latent_size)
-#         self.layer2 = nn.Linear(latent_size, latent_size)
-#         nn.init.eye_(self.layer1.weight)
-#         nn.init.eye_(self.layer2.weight)
-#
-#     def forward(self, x):
-#         x = F.relu(self.layer1(x))
-#         x = F.relu(self.layer2(x))
-#         return x
-#
-#
-# class Discriminator(nn.Module):
-#     def __init__(self, latent_size, data_size):
-#         super(Discriminator, self).__init__()
-#         self.layer1 = nn.Linear(latent_size, math.ceil(math.sqrt(data_size)))
-#         self.layer2 = nn.Linear(math.ceil(math.sqrt(data_size)), 1)
-#         nn.init.kaiming_normal_(self.layer1.weight, mode='fan_in', nonlinearity='relu')
-#         nn.init.kaiming_normal_(self.layer2.weight, mode='fan_in', nonlinearity='sigmoid')
-#
-#     def forward(self, x):
-#         x = F.relu(self.layer1(x))
-#         x = torch.sigmoid(self.layer2(x))
-#         return x
-
-
 class PyODDataset(torch.utils.data.Dataset):
     """Custom Dataset for handling data operations in PyTorch for outlier detection."""
 
