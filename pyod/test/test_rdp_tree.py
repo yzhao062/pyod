650812af34c461b69b0f4fb7e63802b3f0980a92
Sihan Chen <68467006+Yeechin-is-here@users.noreply.github.com>
2024-06-15 23:29:40 -0400

    a

--- pyod/test/test_rdp_tree.py
+++ pyod/test/test_rdp_tree.py
@@ -11,11 +11,11 @@ import torch
 # If pyod is installed, no need to use the following line
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from pyod.models.rdp import RDP_Model
+from pyod.models.rdp_tree import RDPTree
 from pyod.utils.data import generate_data
 
 
-class TestRDP_Model(unittest.TestCase):
+class TestRDPTree(unittest.TestCase):
     """
     Unit tests for the RDP_Model.
     The test is designed for running models only, without any performance check.
@@ -29,19 +29,19 @@ class TestRDP_Model(unittest.TestCase):
 
     def test_initialization(self):
         """Test the initialization and setup of the RDP_Model."""
-        model = RDP_Model(in_c=self.n_features, out_c=self.n_features, dropout_r=0.2)
+        model = RDPTree(in_c=self.n_features, out_c=self.n_features, dropout_r=0.2)
         self.assertIsNotNone(model)
 
     def test_train_model(self):
         """Test training of the RDP_Model."""
-        model = RDP_Model(in_c=self.n_features, out_c=self.n_features, dropout_r=0.2)
+        model = RDPTree(in_c=self.n_features, out_c=self.n_features, dropout_r=0.2)
         X_train, _ = generate_data(n_train=self.n_train, n_features=self.n_features, contamination=self.contamination)
         model.train_model(torch.FloatTensor(X_train), epoch=10)
         self.assertTrue(True)  # Simply check that it completes
 
     def test_evaluate_model(self):
         """Test evaluation of the RDP_Model."""
-        model = RDP_Model(in_c=self.n_features, out_c=self.n_features, dropout_r=0.2)
+        model = RDPTree(in_c=self.n_features, out_c=self.n_features, dropout_r=0.2)
         X_test, _ = generate_data(n_train=self.n_test, n_features=self.n_features, contamination=self.contamination)
         results = model.evaluate_model(torch.FloatTensor(X_test))
         self.assertIn('gap_loss', results)  # Check if results contain expected output
