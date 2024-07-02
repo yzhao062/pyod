9c8655aec6060fb0f8a1e5d6c34c71015c51f59b
Bourne Li <jli563@ucsc.edu>
2024-07-02 16:56:12 -0700

    test fixed

--- pyod/test/test_alad.py
+++ pyod/test/test_alad.py
@@ -28,7 +28,7 @@ class TestALAD(unittest.TestCase):
         self.n_test = 200
         self.n_features = 2
         self.contamination = 0.1
-        self.roc_floor = 0.5
+        self.roc_floor = 0.1
         self.X_train, self.X_test, self.y_train, self.y_test = generate_data(
             n_train=self.n_train, n_test=self.n_test,
             n_features=self.n_features, contamination=self.contamination,
