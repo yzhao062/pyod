3349bd7dee2e38f2e9c6caa3e1cb50629e6f31cd
Yue Zhao <yzhao062@gmail.com>
2024-07-06 22:19:19 -0700

    doc update

--- pyod/test/test_anogan.py
+++ pyod/test/test_anogan.py
@@ -35,95 +35,95 @@ class TestAnoGAN(unittest.TestCase):
 
         self.clf = AnoGAN(epochs=3,
                           contamination=self.contamination)
-    #
-    #     self.clf.fit(self.X_train)
-    #
-    # def test_parameters(self):
-    #     assert (hasattr(self.clf, 'decision_scores_') and
-    #             self.clf.decision_scores_ is not None)
-    #     assert (hasattr(self.clf, 'labels_') and
-    #             self.clf.labels_ is not None)
-    #     assert (hasattr(self.clf, 'threshold_') and
-    #             self.clf.threshold_ is not None)
-    #     assert (hasattr(self.clf, '_mu') and
-    #             self.clf._mu is not None)
-    #     assert (hasattr(self.clf, '_sigma') and
-    #             self.clf._sigma is not None)
-    #
-    # def test_train_scores(self):
-    #     assert_equal(len(self.clf.decision_scores_), self.X_train.shape[0])
-    #
-    # def test_prediction_scores(self):
-    #     pred_scores = self.clf.decision_function(self.X_test)
-    #
-    #     # check score shapes
-    #     assert_equal(pred_scores.shape[0], self.X_test.shape[0])
-    #
-    #     # check performance
-    #     assert (roc_auc_score(self.y_test, pred_scores) >= self.roc_floor)
-    #
-    # def test_prediction_labels(self):
-    #     pred_labels = self.clf.predict(self.X_test)
-    #     assert_equal(pred_labels.shape, self.y_test.shape)
-    #
-    # def test_prediction_proba(self):
-    #     pred_proba = self.clf.predict_proba(self.X_test)
-    #     assert (pred_proba.min() >= 0)
-    #     assert (pred_proba.max() <= 1)
-    #
-    # def test_prediction_proba_linear(self):
-    #     pred_proba = self.clf.predict_proba(self.X_test, method='linear')
-    #     assert (pred_proba.min() >= 0)
-    #     assert (pred_proba.max() <= 1)
-    #
-    # def test_prediction_proba_unify(self):
-    #     pred_proba = self.clf.predict_proba(self.X_test, method='unify')
-    #     assert (pred_proba.min() >= 0)
-    #     assert (pred_proba.max() <= 1)
-    #
-    # def test_prediction_proba_parameter(self):
-    #     with assert_raises(ValueError):
-    #         self.clf.predict_proba(self.X_test, method='something')
-    #
-    # def test_prediction_labels_confidence(self):
-    #     pred_labels, confidence = self.clf.predict(self.X_test,
-    #                                                return_confidence=True)
-    #     assert_equal(pred_labels.shape, self.y_test.shape)
-    #     assert_equal(confidence.shape, self.y_test.shape)
-    #     assert (confidence.min() >= 0)
-    #     assert (confidence.max() <= 1)
-    #
-    # def test_prediction_proba_linear_confidence(self):
-    #     pred_proba, confidence = self.clf.predict_proba(self.X_test,
-    #                                                     method='linear',
-    #                                                     return_confidence=True)
-    #     assert (pred_proba.min() >= 0)
-    #     assert (pred_proba.max() <= 1)
-    #
-    #     assert_equal(confidence.shape, self.y_test.shape)
-    #     assert (confidence.min() >= 0)
-    #     assert (confidence.max() <= 1)
-    #
-    # def test_fit_predict(self):
-    #     pred_labels = self.clf.fit_predict(self.X_train)
-    #     assert_equal(pred_labels.shape, self.y_train.shape)
-    #
-    # def test_fit_predict_score(self):
-    #     self.clf.fit_predict_score(self.X_test, self.y_test)
-    #     self.clf.fit_predict_score(self.X_test, self.y_test,
-    #                                scoring='roc_auc_score')
-    #     self.clf.fit_predict_score(self.X_test, self.y_test,
-    #                                scoring='prc_n_score')
-    #     with assert_raises(NotImplementedError):
-    #         self.clf.fit_predict_score(self.X_test, self.y_test,
-    #                                    scoring='something')
-    #
-    # def test_model_clone(self):
-    #     # for deep models this may not apply
-    #     clone_clf = clone(self.clf)
-    #
-    # def tearDown(self):
-    #     pass
+
+        self.clf.fit(self.X_train)
+
+    def test_parameters(self):
+        assert (hasattr(self.clf, 'decision_scores_') and
+                self.clf.decision_scores_ is not None)
+        assert (hasattr(self.clf, 'labels_') and
+                self.clf.labels_ is not None)
+        assert (hasattr(self.clf, 'threshold_') and
+                self.clf.threshold_ is not None)
+        assert (hasattr(self.clf, '_mu') and
+                self.clf._mu is not None)
+        assert (hasattr(self.clf, '_sigma') and
+                self.clf._sigma is not None)
+
+    def test_train_scores(self):
+        assert_equal(len(self.clf.decision_scores_), self.X_train.shape[0])
+
+    def test_prediction_scores(self):
+        pred_scores = self.clf.decision_function(self.X_test)
+
+        # check score shapes
+        assert_equal(pred_scores.shape[0], self.X_test.shape[0])
+
+        # check performance
+        assert (roc_auc_score(self.y_test, pred_scores) >= self.roc_floor)
+
+    def test_prediction_labels(self):
+        pred_labels = self.clf.predict(self.X_test)
+        assert_equal(pred_labels.shape, self.y_test.shape)
+
+    def test_prediction_proba(self):
+        pred_proba = self.clf.predict_proba(self.X_test)
+        assert (pred_proba.min() >= 0)
+        assert (pred_proba.max() <= 1)
+
+    def test_prediction_proba_linear(self):
+        pred_proba = self.clf.predict_proba(self.X_test, method='linear')
+        assert (pred_proba.min() >= 0)
+        assert (pred_proba.max() <= 1)
+
+    def test_prediction_proba_unify(self):
+        pred_proba = self.clf.predict_proba(self.X_test, method='unify')
+        assert (pred_proba.min() >= 0)
+        assert (pred_proba.max() <= 1)
+
+    def test_prediction_proba_parameter(self):
+        with assert_raises(ValueError):
+            self.clf.predict_proba(self.X_test, method='something')
+
+    def test_prediction_labels_confidence(self):
+        pred_labels, confidence = self.clf.predict(self.X_test,
+                                                   return_confidence=True)
+        assert_equal(pred_labels.shape, self.y_test.shape)
+        assert_equal(confidence.shape, self.y_test.shape)
+        assert (confidence.min() >= 0)
+        assert (confidence.max() <= 1)
+
+    def test_prediction_proba_linear_confidence(self):
+        pred_proba, confidence = self.clf.predict_proba(self.X_test,
+                                                        method='linear',
+                                                        return_confidence=True)
+        assert (pred_proba.min() >= 0)
+        assert (pred_proba.max() <= 1)
+
+        assert_equal(confidence.shape, self.y_test.shape)
+        assert (confidence.min() >= 0)
+        assert (confidence.max() <= 1)
+
+    def test_fit_predict(self):
+        pred_labels = self.clf.fit_predict(self.X_train)
+        assert_equal(pred_labels.shape, self.y_train.shape)
+
+    def test_fit_predict_score(self):
+        self.clf.fit_predict_score(self.X_test, self.y_test)
+        self.clf.fit_predict_score(self.X_test, self.y_test,
+                                   scoring='roc_auc_score')
+        self.clf.fit_predict_score(self.X_test, self.y_test,
+                                   scoring='prc_n_score')
+        with assert_raises(NotImplementedError):
+            self.clf.fit_predict_score(self.X_test, self.y_test,
+                                       scoring='something')
+
+    def test_model_clone(self):
+        # for deep models this may not apply
+        clone_clf = clone(self.clf)
+
+    def tearDown(self):
+        pass
 
 
 if __name__ == '__main__':
