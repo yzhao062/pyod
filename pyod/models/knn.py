4958473403d31f14dd898b759ac61872a73392be
Zhu yizhang <95731595+godarrenw@users.noreply.github.com>
2026-09-16 13:02:00 +0800

    docs: fix dead reference links in KNN/LOF/LUNAR docstrings (#732)
    Signed-off-by: Zhu yizhang <95731595+godarrenw@users.noreply.github.com>

--- pyod/models/knn.py
+++ pyod/models/knn.py
@@ -108,7 +108,7 @@ class KNN(BaseDetector):
         sklearn.metrics.pairwise.pairwise_distances. When p = 1, this is
         equivalent to using manhattan_distance (l1), and euclidean_distance
         (l2) for p = 2. For arbitrary p, minkowski_distance (l_p) is used.
-        See http://scikit-learn.org/stable/modules/generated/sklearn.metrics.pairwise.pairwise_distances
+        See https://scikit-learn.org/stable/modules/generated/sklearn.metrics.pairwise_distances.html
 
     metric_params : dict, optional (default = None)
         Additional keyword arguments for the metric function.
